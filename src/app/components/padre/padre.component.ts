9fafc7bf7bb59fdf871cacaaed1fc4ddf46612c4
Pepe <pepewee07@gmail.com>
2024-05-29 09:34:47 -0500

    OUTPUTS PROGRAMMATICALLY

--- src/app/components/padre/padre.component.ts
+++ src/app/components/padre/padre.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { HijoComponent } from '../hijo/hijo.component';
 
 @Component({
@@ -29,6 +29,32 @@ export class PadreComponent {
   onNotifyAlias(message: string) {
     this.nameAlias = message;
     console.log('Received message from child with alias:', message);
+  }
+
+  // OUTPUTS PROGRAMMATICALLY
+  @ViewChild('childContainer', { read: ViewContainerRef }) container!: ViewContainerRef;
+  private childComponentRef!: ComponentRef<HijoComponent>;
+  createChild() {
+    // Limpiar cualquier componente hijo existente
+    this.container.clear();
+
+    // Crear el componente dinámicamente
+    this.childComponentRef = this.container.createComponent(HijoComponent);
+
+    // Subscribirse al output `eventSendDate`
+    this.childComponentRef.instance.eventSendDate.subscribe((message: string) => {
+      this.addColor(message);
+    });
+
+    // Suscribirse al output `onNameChange`
+    this.childComponentRef.instance.onNameChange.subscribe((message: string) => {
+      this.onNotify(message);
+    });
+
+    // Subscribe to the output `nameChangeAlias`
+    this.childComponentRef.instance.nameChangeAlias.subscribe((message: string) => {
+      this.onNotifyAlias(message);
+    });
   }
 
 }
